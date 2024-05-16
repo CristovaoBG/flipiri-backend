e3fddc85310be7b0c53afa312fd44e401a9e4cfd
Cristovao <cristovao@live.com>
2024-05-16 15:10:33 -0300

    test and implementation sample

--- src/test2.py
+++ src/test2.py
@@ -1,4 +1,4 @@
-from wrapper_class import DataW
+import wrapper_class
 from datetime import datetime
 from autores import *
 from travel import *
@@ -9,19 +9,13 @@ from datetime import datetime
 from bson import ObjectId
 from hosting import *
 
-# info = Info("João Silva", "Masculino")
-# info.save() 
-
-# info = DataW.from_id(ObjectId('6644d002f3a4438f5c50ea3f'),globals())
-# print(info)
-# print('ok')
-
-
+wrapper_class.DEBUG = True  # feio, mas um pouco mais seguro
 
 # cria primeiro autor
 id_feeding = Feeding(datetime(2023, 5, 1), datetime(2023, 5, 15)).save()
 autor = Autores("João Silva", "Masculino", id_feeding)
 id_autor1 = autor.save()
+
 # outro autor
 id_feeding = Feeding(datetime(2023, 5, 1), datetime(2023, 5, 15)).save()
 autor = Autores("Ana Souza", "Feminino", id_feeding)
@@ -32,6 +26,7 @@ id_loc = Location("Auditório", "Rua A, 123").save()
 id_agerange = AgeRange(0,10).save()
 autor_list = [id_autor1]
 id_ac1 = Activity("Palestra", datetime(2023, 5, 10), datetime(2023, 5, 10, 2, 0), autor_list, id_autor1, id_loc, id_agerange, "Educação").save()
+
 # segunda atividade
 id_loc = Location("Pracinha", "Avenida Paulista")
 autor_list = [id_autor1, id_autor2]
@@ -45,7 +40,7 @@ id_trip3 = Trip("rio", "sp", datetime(2025, 8, 10), "avião", 20.0, [id_autor2])
 id_trip4 = Trip("sp", "rio", datetime(2026, 9, 20), "ônibus", 25.0, []).save()
 
 # hospedagem
-id_hosting = Hosting(datetime(2025, 6, 1),datetime(2025, 6, 3), "Naquele Lugar", 5, 50.0, [id_autor1, id_autor2])
+id_hosting = Hosting(datetime(2025, 6, 1),datetime(2025, 6, 3), "Naquele Lugar", 5, 50.0, [id_autor1, id_autor2]).save()
 
-# imprime as coisa tudo 
+# imprime as coisa tudo
 print("ok")
