0866cbc075a5a379ed21e32b967f985354244e20
Cristovao <cristovao@live.com>
2024-05-21 15:04:45 -0300

    fix bugs

--- src/test2.py
+++ src/test2.py
@@ -1,6 +1,6 @@
 import wrapper_class
 from datetime import datetime
-from autores import *
+from authors import *
 from travel import *
 from activity import *
 from pprint import pprint
@@ -13,25 +13,41 @@ wrapper_class.DEBUG = True  # feio, mas um pouco mais seguro
 
 # cria primeiro autor
 id_feeding = Feeding(datetime(2023, 5, 1), datetime(2023, 5, 15)).save()
-autor = Autores("João Silva", "Masculino", id_feeding)
+autor = Authors("João Silva", "Masculino", id_feeding)
 id_autor1 = autor.save()
 
 # outro autor
 id_feeding = Feeding(datetime(2023, 5, 1), datetime(2023, 5, 15)).save()
-autor = Autores("Ana Souza", "Feminino", id_feeding)
+autor = Authors("Ana Souza", "Feminino", id_feeding)
 id_autor2 = autor.save()
 
 # primeira atividade
 id_loc = Location("Auditório", "Rua A, 123").save()
 id_agerange = AgeRange(0,10).save()
 autor_list = [id_autor1]
-id_ac1 = Activity("Palestra", datetime(2023, 5, 10), datetime(2023, 5, 10, 2, 0), autor_list, id_autor1, id_loc, id_agerange, "Educação").save()
+id_ac1 = Activity("Palestra",
+                  datetime(2023, 5, 10),
+                  datetime(2023, 5, 10, 2, 0),
+                  autor_list,
+                  id_autor1,
+                  id_loc,
+                  id_agerange,
+                  "Educação"
+                  ).save()
 
 # segunda atividade
-id_loc = Location("Pracinha", "Avenida Paulista")
+id_loc = Location("Pracinha", "Avenida Paulista").save()
 autor_list = [id_autor1, id_autor2]
-id_agerange = AgeRange(18,100)
-id_ac2 = Activity("Palestra", datetime(2023, 5, 10), datetime(2023, 5, 10, 2, 0), autor_list, id_autor2, id_loc, id_agerange, "Bronhasso").save()
+id_agerange = AgeRange(18,100).save()
+id_ac2 = Activity("Palestra",
+                  datetime(2023, 5, 10),
+                  datetime(2023, 5, 10, 2, 0),
+                  autor_list,
+                  id_autor2,
+                  id_loc,
+                  id_agerange,
+                  "Bronhasso"
+                  ).save()
 
 # viagens
 id_trip1 = Trip("brasilia", "piri", datetime(2025, 6, 1), "carro", 10.0, [id_autor1, id_autor2]).save()
@@ -40,7 +56,13 @@ id_trip3 = Trip("rio", "sp", datetime(2025, 8, 10), "avião", 20.0, [id_autor2])
 id_trip4 = Trip("sp", "rio", datetime(2026, 9, 20), "ônibus", 25.0, []).save()
 
 # hospedagem
-id_hosting = Hosting(datetime(2025, 6, 1),datetime(2025, 6, 3), "Naquele Lugar", 5, 50.0, [id_autor1, id_autor2]).save()
+id_hosting = Hosting(datetime(2025, 6, 1),
+                     datetime(2025, 6, 3),
+                     "Naquele Lugar",
+                     [id_autor1, id_autor2],
+                     5,
+                     500.0
+                     ).save()
 
 # imprime as coisa tudo
 print("ok")
