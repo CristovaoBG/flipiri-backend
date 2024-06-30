bc1d4977c12d7b166700ce5005a2a2bb74a6e05b
Cristovao <cristovao@live.com>
2024-06-29 23:02:23 -0300

    feat: add aditional cost

---
+++ src/aditional_cost.py
@@ -0,0 +1,21 @@
+from bson import ObjectId
+from wrapper_class import DataW
+from dataclasses import asdict, dataclass
+
+@dataclass
+class AditionalCost(DataW):
+    name: str
+    cost: float = 0.0
+    _id: ObjectId = -1
+
+    def simplified_repr(self):
+        return (f'{self.name} *')
+    
+    def validate(self):
+        # verifica se tem um lugar com o mesmo nome
+        same_name_list = DataW.get_items_with_field_value('AditionalCost', 'name', self.name)
+        if same_name_list:
+            if len(same_name_list) > 1 or same_name_list[0]['_id'] != self._id:
+                raise ValueError("Já existe um gasto adicional com este nome")
+
+                
