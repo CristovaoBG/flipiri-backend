607c0dac0a84544033ba60fd69722f5037ee7932
Cristovao <cristovao@live.com>
2024-07-10 14:06:34 -0300

    doc: enhance setup file naming

--- src/debug_setup_script_2.py
+++ src/debug_setup_script_2.py
@@ -44,80 +44,77 @@ id_hosting = Hosting("Pousada padrão", 100, 50.0).save()
 # Criação de autores (simplificado, sem hospedagem)
 autores = {}
 for nome in [
-                "Sarau Literário",
+                "Rafael Correa",
                 "Beto Seabra",
                 "Mauricio", 
                 "Clara Arreguy", 
-                "Aplam", 
+                "Fabrício Gomes", 
                 "Luz Marina", 
                 "Rose Costa", 
                 "Célia", 
                 "Reconto", 
-                "Priscila Sabino e Amanda Luz", 
+                "Priscila Sabino",
+                "Amanda Luz", 
                 "Iris Borges", 
                 "Mediadora: Lariza Amélia Eloi", 
                 "Ana Neila", 
                 "ICAE Comunidade", 
-                "Liduina, Iris, Mauricio e Clara", 
+                "Liduina Bartholo", 
                 "Turma do Caracol", 
                 "Débora Bianca", 
                 "Matrakaberta (Adriana Maciel)",
                 "CHIQUITAS & BACANAS",
                 "Angela",
                 "André Cerino",
-                "Tricontinando (Ana Neila, Rose Costa e Hozana)",
-                "Hozana Costa", "Reconto (Liduina com o Grupo Reconto)",
-                "Eraldo e Angela", 
-                "Iris Borges e Sérgio Pompeo",
+                "Hozana Costa",
+                "Eraldo", 
+                "Sérgio Pompeo",
                 "Maria das Dores Brigagão",
                 "Jacqueline de Mattos",
                 "Raquel", 
                 "Maria Amélia", 
                 "SANEAGO", 
-                "Marcia Lages", 
-                "Turma do Caracol ( Maria e André Cerino)", 
-                "Cinco escritoras mulheres da Flipiri", 
+                "Marcia Lages",  
                 "Angela B. Café", 
-                "Diretoria da Casa de Autores e colaboradores / Marcelo Barra"
+                "Marcelo Barra"
              ]:
     autores[nome] = Authors(nome, "Não especificado", id_hosting, datetime(year=2023,month=6,day=12), datetime(year=2023,month=6,day=16)).save()
 
 # Criação de atividades
 # 12 de Junho
-Activity("Sarau Literário / Lançamento de livros", datetime(2023, 6, 12, 20, 0), datetime(2023, 6, 12, 22, 0), [autores["Sarau Literário"]], autores["Sarau Literário"], id_praca, 0, 100, id_teatro).save()
+Activity("Rafael Correa / Lançamento de livros", datetime(2023, 6, 12, 20, 0), datetime(2023, 6, 12, 22, 0), [autores["Rafael Correa"]], autores["Rafael Correa"], id_praca, 0, 100, id_teatro).save()
 
 # 13 de Junho
 Activity("Mesa de Debates: O que os Livros me Ensinaram", datetime(2023, 6, 13, 11, 0), datetime(2023, 6, 13, 13, 0), [autores["Beto Seabra"]], autores["Beto Seabra"], id_teatro_, 0, 100, id_debate).save()
 Activity("Mesa Russos", datetime(2023, 6, 13, 11, 0), datetime(2023, 6, 13, 13, 0), [autores["Mauricio"]], autores["Mauricio"], id_palco, 0, 100, id_debate).save()
 Activity("Mesa de Debates: O que os Livros trazem de Goiás", datetime(2023, 6, 13, 16, 0), datetime(2023, 6, 13, 18, 0), [autores["Clara Arreguy"]], autores["Clara Arreguy"], id_palco, 0, 100, id_debate).save()
-Activity("Lançamento de livro", datetime(2023, 6, 13, 17, 0), datetime(2023, 6, 13, 19, 0), [autores["Aplam"]], autores["Aplam"], id_tenda, 0, 100, id_evento).save()
-Activity("Cerimônia de Abertura", datetime(2023, 6, 13, 19, 0), datetime(2023, 6, 13, 20, 0), [autores["Aplam"]], autores["Aplam"], id_pousada, 0, 100, id_evento).save()
+Activity("Lançamento de livro", datetime(2023, 6, 13, 17, 0), datetime(2023, 6, 13, 19, 0), [autores["Fabrício Gomes"]], autores["Fabrício Gomes"], id_tenda, 0, 100, id_evento).save()
+Activity("Cerimônia de Abertura", datetime(2023, 6, 13, 19, 0), datetime(2023, 6, 13, 20, 0), [autores["Fabrício Gomes"]], autores["Fabrício Gomes"], id_pousada, 0, 100, id_evento).save()
 
 # 14 de Junho
 Activity("Trupe dos Cirandeiros", datetime(2023, 6, 14, 8, 0), datetime(2023, 6, 14, 22, 0), [autores["Luz Marina"]], autores["Luz Marina"], id_espaco, 0, 100, id_teatro).save()
 Activity("Pina literária", datetime(2023, 6, 14, 10, 0), datetime(2023, 6, 14, 11, 0), [autores["Rose Costa"]], autores["Rose Costa"], id_igreja, 0, 100, id_evento).save()
 Activity("Espetáculo", datetime(2023, 6, 14, 9, 30), datetime(2023, 6, 14, 10, 30), [autores["Célia"]], autores["Célia"], id_musica, 0, 100, id_teatro).save()
 Activity("Espetáculo", datetime(2023, 6, 14, 10, 0), datetime(2023, 6, 14, 11, 0), [autores["Reconto"]], autores["Reconto"], id_musica2, 0, 100, id_teatro).save()
-Activity("Musicalização", datetime(2023, 6, 14, 10, 0), datetime(2023, 6, 14, 10, 30), [autores["Priscila Sabino e Amanda Luz"]], autores["Priscila Sabino e Amanda Luz"], id_bebeteca, 0, 5, id_evento).save()
+Activity("Musicalização", datetime(2023, 6, 14, 10, 0), datetime(2023, 6, 14, 10, 30), [autores["Priscila Sabino"], autores["Amanda Luz"]], autores["Priscila Sabino"], id_bebeteca, 0, 5, id_evento).save()
 Activity("Contação de histórias para bebês", datetime(2023, 6, 14, 10, 30), datetime(2023, 6, 14, 11, 0), [autores["Iris Borges"]], autores["Iris Borges"], id_bebeteca, 0, 5, id_itinerancia).save()
 Activity("Mesa de Debates: Literatura e Ascensão Social", datetime(2023, 6, 14, 11, 0), datetime(2023, 6, 14, 13, 0), [autores["Mediadora: Lariza Amélia Eloi"]], autores["Mediadora: Lariza Amélia Eloi"], id_auditorio, 0, 100, id_debate).save()
 Activity("Contação de histórias - Bebeteca", datetime(2023, 6, 14, 11, 0), datetime(2023, 6, 14, 11, 30), [autores["Ana Neila"]], autores["Ana Neila"], id_bebeteca, 0, 12, id_itinerancia).save()
 Activity("Concentração do cortejo", datetime(2023, 6, 14, 14, 0), datetime(2023, 6, 14, 15, 0), [autores["ICAE Comunidade"]], autores["ICAE Comunidade"], id_tenda, 0, 100, id_evento).save()
 Activity("Cortejo", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 0), [autores["ICAE Comunidade"]], autores["ICAE Comunidade"], id_saida, 0, 100, id_evento).save()
-Activity("Oficinas: Escolas Estaduais", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 0), [autores["Liduina, Iris, Mauricio e Clara"]], autores["Liduina, Iris, Mauricio e Clara"], id_tenda, 0, 100, id_evento).save()
-Activity("Espetáculo - Palco Bebeteca", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 0), [autores["Turma do Caracol"]], autores["Turma do Caracol"], id_palco, 0, 12, id_teatro).save()
+Activity("Oficinas: Escolas Estaduais", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 0), [autores["Liduina Bartholo"], autores["Iris Borges"], autores["Mauricio"], autores["Clara Arreguy"]], autores["Iris Borges"], id_tenda, 0, 100, id_evento).save()
 Activity("Contação de histórias, brincadeiras e canções - Bebeteca", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 0), [autores["Débora Bianca"]], autores["Débora Bianca"], id_musica, 0, 5, id_evento).save()
-Activity("Musicalização para bebês - Bebeteca", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 20), [autores["Priscila Sabino e Amanda Luz"]], autores["Priscila Sabino e Amanda Luz"], id_bebeteca, 0, 5, id_teatro).save()
+Activity("Musicalização para bebês - Bebeteca", datetime(2023, 6, 14, 15, 0), datetime(2023, 6, 14, 16, 20), [autores["Priscila Sabino"], autores["Amanda Luz"]], autores["Amanda Luz"], id_bebeteca, 0, 5, id_teatro).save()
 Activity("Contação de histórias - Bebeteca", datetime(2023, 6, 14, 16, 30), datetime(2023, 6, 14, 17, 30), [autores["Matrakaberta (Adriana Maciel)"]], autores["Matrakaberta (Adriana Maciel)"], id_palco, 0, 12, id_teatro).save()
 Activity("Show Musical", datetime(2023, 6, 14, 20, 30), datetime(2023, 6, 14, 22, 30), [autores["CHIQUITAS & BACANAS"], autores["Angela"]], autores["CHIQUITAS & BACANAS"], id_praca, 0, 100, id_evento).save()
 
 # 15 de Junho
 Activity("Oficina de ilustração + Mesa - Encontro de Ilustradores", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 12, 0), [autores["André Cerino"]], autores["André Cerino"], id_tenda2, 0, 100, id_evento).save()
-Activity("Leitura, Brincadeiras e Canções - Bebeteca", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 10, 0), [autores["Tricontinando (Ana Neila, Rose Costa e Hozana)"]], autores["Tricontinando (Ana Neila, Rose Costa e Hozana)"], id_palco, 0, 5, id_evento).save()
+Activity("Leitura, Brincadeiras e Canções - Bebeteca", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 10, 0), [autores["Ana Neila"], autores["Rose Costa"], autores["Hozana Costa"]], autores["Rose Costa"], id_palco, 0, 5, id_evento).save()
 Activity("Contação de histórias - Bebeteca", datetime(2023, 6, 15, 10, 20), datetime(2023, 6, 15, 11, 0), [autores["Hozana Costa"]], autores["Hozana Costa"], id_bebeteca, 0, 5, id_itinerancia).save()
-Activity("Espetáculo", datetime(2023, 6, 15, 10, 0), datetime(2023, 6, 15, 11, 0), [autores["Reconto (Liduina com o Grupo Reconto)"]], autores["Reconto (Liduina com o Grupo Reconto)"], id_palco, 0, 5, id_teatro).save()
-Activity("Uns CAUSOS", datetime(2023, 6, 15, 11, 0), datetime(2023, 6, 15, 12, 0), [autores["Eraldo e Angela"]], autores["Eraldo e Angela"], id_teatro_, 0, 100, id_teatro).save()
-Activity("Walking Tour", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 11, 0), [autores["Iris Borges e Sérgio Pompeo"]], autores["Iris Borges e Sérgio Pompeo"], id_caminhada, 0, 100, id_evento).save()
+Activity("Espetáculo", datetime(2023, 6, 15, 10, 0), datetime(2023, 6, 15, 11, 0), [autores["Reconto"], autores["Liduina Bartholo"]], autores["Reconto"], id_palco, 0, 5, id_teatro).save()
+Activity("Uns CAUSOS", datetime(2023, 6, 15, 11, 0), datetime(2023, 6, 15, 12, 0), [autores["Eraldo"], autores["Angela"]], autores["Angela"], id_teatro_, 0, 100, id_teatro).save()
+Activity("Walking Tour", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 11, 0), [autores["Iris Borges"], autores["Sérgio Pompeo"]], autores["Iris Borges"], id_caminhada, 0, 100, id_evento).save()
 Activity("Oficina - A Fantástica Matemática", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 11, 0), [autores["Maria das Dores Brigagão"]], autores["Maria das Dores Brigagão"], id_colegio, 0, 100, id_evento).save()
 Activity("Oficina - A literatura e suas múltiplas possibilidades", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 11, 0), [autores["Jacqueline de Mattos"]], autores["Jacqueline de Mattos"], id_colegio2, 0, 100, id_evento).save()
 Activity("Oficina - Leitura para a Cultura de Paz", datetime(2023, 6, 15, 9, 0), datetime(2023, 6, 15, 11, 0), [autores["Débora Bianca"]], autores["Débora Bianca"], id_colegio3, 0, 100, id_evento).save()
@@ -128,16 +125,16 @@ Activity("Roda de conversa", datetime(2023, 6, 15, 14, 30), datetime(2023, 6, 15
 Activity("Espetáculo - Bebeteca", datetime(2023, 6, 15, 15, 0), datetime(2023, 6, 15, 16, 0), [autores["Matrakaberta (Adriana Maciel)"]], autores["Matrakaberta (Adriana Maciel)"], id_palco, 0, 5, id_teatro).save()
 Activity("Contação de histórias - Bebeteca", datetime(2023, 6, 15, 15, 0), datetime(2023, 6, 15, 15, 30), [autores["Hozana Costa"]], autores["Hozana Costa"], id_bebeteca, 0, 5, id_itinerancia).save()
 Activity("Contação de histórias - Bebeteca", datetime(2023, 6, 15, 15, 30), datetime(2023, 6, 15, 16, 0), [autores["Jacqueline de Mattos"]], autores["Jacqueline de Mattos"], id_bebeteca, 0, 5, id_itinerancia).save()
-Activity("Show - Bebeteca", datetime(2023, 6, 15, 16, 30), datetime(2023, 6, 15, 17, 30), [autores["Turma do Caracol ( Maria e André Cerino)"]], autores["Turma do Caracol ( Maria e André Cerino)"], id_palco, 0, 5, id_evento).save()
-Activity("Mesa de debates: o livro das Nossas Vidas", datetime(2023, 6, 15, 16, 0), datetime(2023, 6, 15, 18, 0), [autores["Cinco escritoras mulheres da Flipiri"], autores["Mediadora: Lariza Amélia Eloi"]], autores["Mediadora: Lariza Amélia Eloi"], id_teatro2, 0, 100, id_debate).save()
+Activity("Show - Bebeteca", datetime(2023, 6, 15, 16, 30), datetime(2023, 6, 15, 17, 30), [autores["Turma do Caracol"]], autores["Turma do Caracol"], id_palco, 0, 5, id_evento).save()
+Activity("Mesa de debates: o livro das Nossas Vidas", datetime(2023, 6, 15, 16, 0), datetime(2023, 6, 15, 18, 0), [autores["Mediadora: Lariza Amélia Eloi"]], autores["Mediadora: Lariza Amélia Eloi"], id_teatro2, 0, 100, id_debate).save()
 Activity("Show de Talentos", datetime(2023, 6, 15, 17, 0), datetime(2023, 6, 15, 20, 0), [autores["Angela B. Café"]], autores["Angela B. Café"], id_praca, 0, 100, id_evento).save()
-Activity("Cerimônia de Encerramento", datetime(2023, 6, 15, 20, 30), datetime(2023, 6, 15, 22, 30), [autores["Diretoria da Casa de Autores e colaboradores / Marcelo Barra"]], autores["Diretoria da Casa de Autores e colaboradores / Marcelo Barra"], id_praca, 0, 100, id_evento).save()
+Activity("Cerimônia de Encerramento", datetime(2023, 6, 15, 20, 30), datetime(2023, 6, 15, 22, 30), [autores["Marcelo Barra"]], autores["Marcelo Barra"], id_praca, 0, 100, id_evento).save()
 
 id_trip1 = Trip("brasilia", "piri", datetime(2025, 6, 1), "aviao", 10.0, [autores["Turma do Caracol"]]).save()
 id_trip1 = Trip("piri", "brasilia", datetime(2025, 6, 10), "aviao", 10.0, [autores["Turma do Caracol"]]).save()
 id_trip1 = Trip("brasilia", "groenlândia", datetime(2025, 6, 11), "aviao", 10.0, [autores["Turma do Caracol"]]).save()
-id_trip1 = Trip("brasilia", "piri", datetime(2025, 6, 1), "carro", 10.0, [autores["Clara Arreguy"],autores["Aplam"],]).save()
-id_trip1 = Trip("piri", "brasilia", datetime(2025, 6, 10), "carro", 10.0, [autores["Clara Arreguy"],autores["Aplam"],autores["Rose Costa"]]).save()
+id_trip1 = Trip("brasilia", "piri", datetime(2025, 6, 1), "carro", 10.0, [autores["Clara Arreguy"],autores["Fabrício Gomes"],]).save()
+id_trip1 = Trip("piri", "brasilia", datetime(2025, 6, 10), "carro", 10.0, [autores["Clara Arreguy"],autores["Fabrício Gomes"],autores["Rose Costa"]]).save()
 
 
 AditionalCost("ABERTURA", 1000.0).save()
@@ -160,5 +157,7 @@ AditionalCost("LIMPEZA", 1000.0).save()
 AditionalCost("RECEPÇÃO", 1600.0).save()
 AditionalCost("LOCAÇÃO DE CADEIRAS", 500.0).save()
 AditionalCost("IMPOSTO ICA", 3160.0).save()
+
+DataW.set_meal_price(10.5)
 
 print("ok")
